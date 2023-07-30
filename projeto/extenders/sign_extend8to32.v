f9d16c517d5e317bb96d46c428690aa619320b1f
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2023-07-30 12:04:46 -0300

    Delete sign_extend8to32.v

--- projeto/extenders/sign_extend8to32.v
+++
@@ -1,7 +0,0 @@
-module sign_extend_8to32(input wire [7:0] input_data, 
-                         output wire [31:0] output_data);
-
-  // Expande os 8 bits de entrada para 32 bits adicionando 24 bits com o mesmo valor do bit de sinal
-  assign output_data = (input_data[7]) ? {{8{1'b1}}, input_data} : {{8{1'b0}}, input_data};
-  
-end module
