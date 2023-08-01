16b931ac74e41528c40d33ee97362461f0f00e98
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2023-08-01 20:56:15 -0300

    Correct logic_extend8to32.v

--- projeto/extenders/logic_extend8to32.v
+++ projeto/extenders/logic_extend8to32.v
@@ -4,4 +4,4 @@ module logic_extend_8to32(input wire [7:0] input_data,
   // Expande os 8 bits de entrada para 32 bits adicionando 24 bits com o valor 0
   assign output_data = {{24{1'b0}}, data_1};;
 
-end module 
+endmodule 
