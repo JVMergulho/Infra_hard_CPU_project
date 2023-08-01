83a0bac72fb308d96884ab3637b0167ec8407f3a
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2023-08-01 20:56:31 -0300

    Correct logic_extend1to32.v

--- projeto/extenders/logic_extend1to32.v
+++ projeto/extenders/logic_extend1to32.v
@@ -4,4 +4,4 @@ module logic_extend_1to32(input wire input_data,
   // Expande o único bit de entrada para 32 bits adicionando 31 bits com o valor 0
   assign output_data ={{31'b0}, input_data};
   
-end module
+endmodule
