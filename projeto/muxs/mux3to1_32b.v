334e32662413871a54e91aff3713913891ec63aa
anasosl <108680344+anasosl@users.noreply.github.com>
2023-08-02 13:03:47 -0300

    Update mux3to1_32b.v
    output reg -> wire

--- projeto/muxs/mux3to1_32b.v
+++ projeto/muxs/mux3to1_32b.v
@@ -8,7 +8,7 @@ module mux3to1_32b(input wire [31:0] input00_0,
 	           input wire [31:0] input01_1, 
 	           input wire [31:0] input10_2, 
                    input wire [1:0] sel, 
-                   output reg [31:0] out);
+                   output wire [31:0] out);
 
  
   assign out = (sel == 2'b00) ? input00_0 : 
