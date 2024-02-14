bff2e0e66abe3e7ba2024ec2d546e88f7df06d68
PedroMonte <pedrovitor23.monte@gmail.com>
2024-02-13 23:13:34 -0300

    Revert "corrigido: versão mais atual"
    This reverts commit c2947ab2db3ebd34388f08c32943c80de07475a6.

--- Shifterleft32.v
+++
@@ -1,8 +0,0 @@
-module Shifterleft32(
-  input wire [31:0] data_in,
-  output wire [31:0] data_out
-);
-
-  assign data_out = data_in << 2;
-
-endmodule
--- load_unit.v
+++
@@ -1,9 +0,0 @@
-module load_unit (
-  input wire [31:0] mem_in, 
-  input wire [1:0] LoadCtrl,
-  output wire [31:0] data_out
-);
-
-  assign data_out = (LoadCtrl == 2'b01) ? mem_in : (LoadCtrl == 2'b10) ? {{16{1'b0}},mem_in[15:0]} : {{23{1'b0}},mem_in[7:0]};
-
-endmodule
